d9d195ba2a96c4db23da47a8c82c7ef93b75bb9c
ShaginIlia <166448106+ShaginIlia@users.noreply.github.com>
2024-04-27 00:33:51 +0300

    Add files via upload

---
+++ HW9.py
@@ -0,0 +1,8 @@
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in range(len(cars)):
+    print('Я езжу на автомоболе марки', cars[i])
+
+cars_count = 0
+for i in range(len(cars)):
+    cars_count += 10
+print(cars_count)
