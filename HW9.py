a19ce81b9af318981dfb9c535a28cef487e41ee9
ShaginIlia <166448106+ShaginIlia@users.noreply.github.com>
2024-04-27 12:06:37 +0300

    Add files via upload

--- HW9.py
+++ HW9.py
@@ -1,8 +1,6 @@
 cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
-for i in range(len(cars)):
-    print('Я езжу на автомоболе марки', cars[i])
-
 cars_count = 0
 for i in range(len(cars)):
     cars_count += 10
+    print('Я езжу на автомоболе марки', cars[i])
 print(cars_count)
